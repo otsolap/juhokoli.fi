1e67c4a5a6ded212c1bacf7ebf3e97016396366f
OtsoLap <otsolappalainen@gmail.com>
2022-03-16 09:13:45 +0200

    Footer settings for backend and adding fontawesome to footer.

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,17 @@
+import { faLinkedin, faInstagram, faTwitter, } from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
     return (
-        <footer>Footer Built by me!</footer>
+        <footer>Footer Built by me!
+            <p>Testing FA Icons:</p>
+            <FontAwesomeIcon aria-label="LinkedIn profile" icon={faInstagram} />
+            <FontAwesomeIcon aria-label="LinkedIn profile" icon={faTwitter} />
+            <FontAwesomeIcon aria-label="LinkedIn profile" icon={faLinkedin} />
+            <FontAwesomeIcon aria-label="LinkedIn profile" icon={faEnvelope} />
+            <FontAwesomeIcon aria-label="LinkedIn profile" icon={faPhone} />
+        </footer>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '../styles/globals.css'
 import Layout from '@components/Layout'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
