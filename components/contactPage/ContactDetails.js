b4e0332966903a06e748704b8fca0c51a2fff7d5
OtsoLap <otsolappalainen@gmail.com>
2022-03-18 12:33:51 +0200

    Contact page backend logic done.

--- components/contactPage/ContactDetails.js
+++ components/contactPage/ContactDetails.js
@@ -6,30 +6,10 @@ const ContactDetails = ({ contact }) => {
     const SoMe = contact.socialMedia.map((icons, i) => {
         return (
             <span className="some-icons" key={i}>
-                {icons.icon === 'Instagram' ? (
-                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon aria-label="Instagram profile" icon={faInstagram} />
-                    </a>
-                ) : (
-                    ""
-                )}
-                {icons.icon === 'Twitter' ? (
-                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon aria-label="Twitter profile" icon={faTwitter} />
-                    </a>
-                ) : (
-                    ""
-                )}
-                {icons.icon === 'LinkedIn' ? (
-                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon aria-label="LinkedIn profile" icon={faLinkedin} />
-                    </a>
-                ) : (
-                    ""
-                )}
                 {icons.icon === 'Email' ? (
                     <a href={`mailto:${icons.url}`} target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon aria-label="Email" icon={faEnvelope} />
+                        <span className="some-text">{icons.url}</span>
                     </a>
                 ) : (
                     ""
@@ -37,6 +17,31 @@ const ContactDetails = ({ contact }) => {
                 {icons.icon === 'Phone' ? (
                     <a href={`tel:${icons.url}`} target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon aria-label="Phone" icon={faPhone} />
+                        <span className="some-text">{icons.url}</span>
+                    </a>
+                ) : (
+                    ""
+                )}
+                {icons.icon === 'Instagram' ? (
+                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon aria-label="Instagram profile" icon={faInstagram} />
+                        <span className="some-text">{icons.description}</span>
+                    </a>
+                ) : (
+                    ""
+                )}
+                {icons.icon === 'Twitter' ? (
+                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon aria-label="Twitter profile" icon={faTwitter} />
+                        <span className="some-text">{icons.description}</span>
+                    </a>
+                ) : (
+                    ""
+                )}
+                {icons.icon === 'LinkedIn' ? (
+                    <a href={icons.url} target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon aria-label="LinkedIn profile" icon={faLinkedin} />
+                        <span className="some-text">{icons.description}</span>
                     </a>
                 ) : (
                     ""
