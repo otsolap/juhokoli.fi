78db9844dd2c3da56ec8f0a48b9c2bb8d09fd87c
OtsoLap <otsolappalainen@gmail.com>
2022-04-08 20:35:22 +0300

    Avoine teachings applied to Koli.fi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1366px',
+      '3xl': '1440px',
+    },
     extend: {
       colors: {
         primary: '#0525e2',
@@ -11,7 +20,14 @@ module.exports = {
         aquamarine: '#69dee3',
         white: '#ffffff',
         grey: '#f2f2f2',
-      }
+      },
+      spacing: {
+        half: '.5rem',
+        single: '1rem',
+        double: '2rem',
+        triple: '3rem',
+        14: '3.5rem',
+      },
     },
   },
   plugins: [],
