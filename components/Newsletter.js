8729bb722ed5e5b08331b342dfedda66ce3a68ee
OtsoLap <otsolappalainen@gmail.com>
2022-04-11 22:54:08 +0300

    tailwind progress

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,9 +1,33 @@
 import Image from 'next/image'
+import styles from '../styles/Newsletter.module.css'
 
 const Newsletter = ({ newsletter }) => {
+
+    const registerNewsletter = async e => {
+        e.preventDefault()
+
+        const res = await fetch(
+            'https://hooks.zapier.com/hooks/catch/123456/abcde',
+            {
+                body: JSON.stringify({
+                    email: e.target.email.value
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST'
+            }
+        )
+
+        const result = await res.json()
+        // result.user => 'Ada Lovelace'
+
+    }
+
+
     return (
         <section id="newsletter">
-            <div className="container newsletter-content">
+            <div className={styles.newsletterContent}>
                 <Image
                     src={newsletter.backgroundImage}
                     layout="fill"
@@ -11,16 +35,28 @@ const Newsletter = ({ newsletter }) => {
                     quality={100}
                     objectPosition="center"
                     alt={newsletter.title}
-                    className="newsletter-bg-img"
+                    className={styles.newsletterBGImg}
                 />
-                <div className="content">
+                <div className={styles.content}>
                     <h2>{newsletter.title}</h2>
                     <p>{newsletter.description}</p>
-                    <form className="newsletterForm" action="http://www.acme.com/register" method="POST">
-                        <label for="name">Sähköposti</label>
-                        <input id="email" type="email" autocomplete="email" required />
-                        <button classname="btn" type="submit">Tilaa uutiskirje</button>
-                    </form>
+                    <div className={styles.formContainer}>
+                        <form className="newsletterForm" onSubmit={registerNewsletter}>
+                            <input
+                                placeholder="Sähköposti* "
+                                id="newsletterEmail"
+                                name="newsletterEmail"
+                                type="email"
+                                autoComplete="email"
+                                required
+                            />
+                            <button
+                                className={styles.btn}
+                                type="submit">
+                                Tilaa uutiskirje
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </section>
