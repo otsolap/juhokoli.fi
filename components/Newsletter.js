5fedc1dad90067a5c101c1ddd252820c26263e9b
OtsoLap <otsolappalainen@gmail.com>
2022-04-11 22:17:16 +0300

    Newsletter bg-image done. Now for styling and content.

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -3,23 +3,24 @@ import Image from 'next/image'
 const Newsletter = ({ newsletter }) => {
     return (
         <section id="newsletter">
-            <div className="background-image-container">
+            <div className="container newsletter-content">
                 <Image
                     src={newsletter.backgroundImage}
-                    /*             layout="fill" */
-                    height={500}
-                    width={390}
+                    layout="fill"
                     objectFit='cover'
                     quality={100}
                     objectPosition="center"
                     alt={newsletter.title}
                     className="newsletter-bg-img"
                 />
-            </div>
-            <div className="container">
-                <h2>{newsletter.title}</h2>
                 <div className="content">
+                    <h2>{newsletter.title}</h2>
                     <p>{newsletter.description}</p>
+                    <form className="newsletterForm" action="http://www.acme.com/register" method="POST">
+                        <label for="name">Sähköposti</label>
+                        <input id="email" type="email" autocomplete="email" required />
+                        <button classname="btn" type="submit">Tilaa uutiskirje</button>
+                    </form>
                 </div>
             </div>
         </section>
