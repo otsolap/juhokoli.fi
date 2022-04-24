63dfa7868dac5bd0324cb7e7b05a2a2c9aa651d0
OtsoLap <otsolappalainen@gmail.com>
2022-04-24 08:21:03 +0300

    flex fixes on faq

--- components/aboutMePage/Faq.js
+++ components/aboutMePage/Faq.js
@@ -8,23 +8,25 @@ const Faq = ({ faq }) => {
 
     return (
         <section id={styles.faqSection}>
-            <div className={`desktop-only ${styles.profilePic}`}>
-                <Image
-                    src={faq.faqProfilePicture}
-                    alt={faq.faqProfilePictureAlt}
-                    width={850}
-                    height={1240}
-                    quality={100}
-                />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.content}>
-                    {faq.faqs.map((faq, i) => (
-                        <Question
-                            key={i}
-                            {...faq}
-                        />
-                    ))}
+            <div className={`container ${styles.faqSectionContainer}`}>
+                <div className={`desktop-only ${styles.profilePic}`}>
+                    <Image
+                        src={faq.faqProfilePicture}
+                        alt={faq.faqProfilePictureAlt}
+                        width={850}
+                        height={1240}
+                        quality={100}
+                    />
+                </div>
+                <div className={styles.container}>
+                    <div className={styles.content}>
+                        {faq.faqs.map((faq, i) => (
+                            <Question
+                                key={i}
+                                {...faq}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
