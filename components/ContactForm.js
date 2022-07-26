547a798c536520db0a9eaf016c2cb7f5f14aac40
OtsoLap <otsolappalainen@gmail.com>
2022-07-26 17:50:45 +0300

    refactor wip

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,97 +1,97 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import styles from '../styles/Contact.module.scss'
 
 const ContactForm = ({ contact }) => {
-    const handleSubmit = async (event) => {
-        event.preventDefault()
 
-        const data = {
-            fullName: event.target.fullName.value,
-            tel: event.target.tel.value,
-            email: event.target.email.value,
-            message: event.target.message.value,
-        }
+    const [submitterName, setSubmitterName] = useState("");
+    const router = useRouter();
+    const confirmationScreenVisible =
+        router.query?.success && router.query.success === "true";
+    const formVisible = !confirmationScreenVisible;
 
-        const JSONdata = JSON.stringify(data)
+    const ConfirmationMessage = (
+        <React.Fragment>
+            <p>
+                Thank you for submitting this form. Someone should get back to you
+                within 24-48 hours.
+            </p>
 
-        const endpoint = '/api/contactForm'
+            <button
+                onClick={() => router.replace("/", undefined, { shallow: true })}
+            >
+                Submit Another Response
+            </button>
+        </React.Fragment>
+    );
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSONdata,
-        }
+    const theContactForm = (
+        <form
+            name="contact-form"
+            method="POST"
+            action="/?success=true"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+        >
+            <input type="hidden" name="subject" value={`Prospekti ${submitterName} otti yhteyttä Juhokoli.fi`} />
+            <input type="hidden" name="contact-form" value="contact-form" />
+            <input type="hidden" name="bot-field" />
+            <div className={styles.formControl}>
+                <label htmlFor="fullName">Nimi *</label>
+                <input
+                    placeholder="Nimi *"
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    autoComplete="name"
+                    onChange={(e) => setSubmitterName(e.target.value)}
+                    required
+                />
+            </div>
+            <div className={styles.formControl}>
+                <label htmlFor="tel">Puhelinnumero *</label>
+                <input
+                    placeholder="Puhelinnumero *"
+                    type="tel"
+                    autoComplete="tel"
+                    name="tel"
+                    id="tel"
+                    required
+                />
+            </div>
+            <div className={styles.formControl}>
+                <label htmlFor="email">Sähköposti</label>
+                <input
+                    placeholder="Sähköposti"
+                    type="email"
+                    autoComplete="email"
+                    name="email"
+                    id="email"
+                />
+            </div>
+            <div className={styles.formControl}>
+                <label htmlFor="message">Viesti</label>
+                <textarea
+                    placeholder="Viesti"
+                    name="message"
+                    id="message"
+                />
+            </div>
+            <div className={styles.formControl}>
+                <button
+                    type="submit"
+                    className="btn">
+                    {contact.CTA}
+                </button>
+            </div>
+        </form>
+    )
 
-        // Send the form data to our forms API on Vercel and get a response.
-        const response = await fetch(endpoint, options)
-
-        // Get the response data from server as JSON.
-        // If server returns the name submitted, that means the form works.
-        const result = await response.json()
-        console.log(result)
-    }
 
     return (
         <section id={styles.formSection}>
             <div id="yhteydenotto" className={styles.formContainer}>
-                <form
-                    data-netlify="true"
-                    netlify-honeypot="bot-field"
-                    onSubmit={handleSubmit}
-                    name="contact"
-                >
-                    <input type="hidden" name="subject" value="Prospekti yhteydenotto Juhokoli.fi" />
-                    <input type="hidden" name="bot-field" />
-                    <input type="hidden" name="contact" value="contact" />
-                    <div className={styles.formControl}>
-                        <label htmlFor="fullName">Nimi *</label>
-                        <input
-                            placeholder="Nimi *"
-                            type="text"
-                            name="fullName"
-                            id="fullName"
-                            autoComplete="name"
-                            required
-                        />
-                    </div>
-                    <div className={styles.formControl}>
-                        <label htmlFor="tel">Puhelinnumero *</label>
-                        <input
-                            placeholder="Puhelinnumero *"
-                            type="tel"
-                            autoComplete="tel"
-                            name="tel"
-                            id="tel"
-                            required
-                        />
-                    </div>
-                    <div className={styles.formControl}>
-                        <label htmlFor="email">Sähköposti</label>
-                        <input
-                            placeholder="Sähköposti"
-                            type="email"
-                            autoComplete="email"
-                            name="email"
-                            id="email"
-                        />
-                    </div>
-                    <div className={styles.formControl}>
-                        <label htmlFor="message">Viesti</label>
-                        <textarea
-                            placeholder="Viesti"
-                            name="message"
-                            id="message"
-                        />
-                    </div>
-                    <div className={styles.formControl}>
-                        <button
-                            type="submit"
-                            className="btn">
-                            {contact.CTA}
-                        </button>
-                    </div>
-                </form>
+                {formVisible ? theContactForm : ConfirmationMessage}
             </div>
         </section>
     )
